2cf0a530f8bb018954582b97854962b66391b99f
Kirill Ivanov <xfiendx4life@gmail.com>
2016-07-16 17:21:56 +0300

    Almost parsed xml. Problems with full text

---
+++ get_data.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import requests
+import xml.etree.ElementTree as ET
+
+def get_xml():
+    r = requests.get('http://sntat.ru/rss/yandex_news.xml')
+    r.encoding = 'utf-8'
+    return r.text
+
+def parse_xml():
+    text = get_xml()
+    root = ET.fromstring(text)
+    news = []
+    for child in root[0]:
+        if child.tag.split("}")[1] == 'item':
+            #print(child[0].tag)
+            news_dict = {}
+            news_dict['title'] = child.find('{http://backend.userland.com/rss2}title').text
+            news_dict['link'] = child.find('{http://backend.userland.com/rss2}link').text
+            news_dict['pdalink'] = child.find('{http://backend.userland.com/rss2}pdalink').text
+            news_dict['enclosure'] = child.find('{http://backend.userland.com/rss2}enclosure').attrib #check
+            news_dict['description'] = child.find('{http://backend.userland.com/rss2}description').text
+            news_dict['category'] = child.find('{http://backend.userland.com/rss2}category').text
+            #news_dict['yandex:genre'] = child.find('{http://backend.userland.com/rss2}yandex:genre').text
+            news_dict['pubDate'] = child.find('{http://backend.userland.com/rss2}pubDate').text
+            #news_dict['yandex:full-text'] = child.find('{http://backend.userland.com/rss2}yandex:full-text').text
+            news.append(news_dict)
+    return news
+
+def message_maker():
+    news = parse_xml()
+    message = 'Главные события на этот час: \n'
+    for i in range(5):
+        message += '%s \n (%s)' % (news[i]['title'],news[i]['link'])
+    return message
