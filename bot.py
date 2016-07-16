e4931bc446e15164cf5226822620e0ee22f50967
Kirill Ivanov <xfiendx4life@gmail.com>
2016-07-16 15:55:34 +0300

    Initial news commit

---
+++ bot.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import config
+import telebot
+from telebot import types
+import os
+
+bot = telebot.TeleBot(config.token)
+day = ''
+
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.send_message(message.chat.id, 'Хочешь немного новостей?')
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    #markup = types.ForceReply(selective=False)
+    bot.send_message(message.chat.id, 'Пока работаем над вашими новостями')
+
+
+
+if __name__ == '__main__':
+    try:
+        bot.polling(none_stop=True)
+    except:
+        os.startfile('bot.py')
+        print("Here's an ERROR")
---
+++ config.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+token = '167828695:AAGxu1bYyMbt2_juzcVyWTfAbK354q6eCWk'
